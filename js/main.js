3a8b47f32c10629d7447f42e7d126b39aeb1f788
Tanner Smith <tanner@tssoftware.net>
2012-12-23 17:25:10 -0500

    Add arc attribute.
    From Raphael Polar clock example.

--- js/main.js
+++ js/main.js
@@ -4,4 +4,28 @@ $(document).ready(function() {
 	var height = $(window).height() - $('.container').outerHeight() - 80;
 
 	paper = Raphael('paper', $(window).width(), height);
+
+	paper.customAttributes.arc = function (xloc, yloc, value, total, R) {
+		var alpha = 360 / total * value;
+		var a = (90 - alpha) * Math.PI / 180;
+		var x = xloc + R * Math.cos(a);
+		var y = yloc - R * Math.sin(a);
+		var path;
+
+		if (total == value) {
+			path = [
+				["M", xloc, yloc - R],
+				["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
+			];
+		} else {
+			path = [
+				["M", xloc, yloc - R],
+				["A", R, R, 0, +(alpha > 180), 1, x, y]
+			];
+		}
+
+		return {
+			path: path
+		};
+	};
 });
