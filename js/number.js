b7d8dd6c0d0106166be12af69e1a7edcefd7d60f
Tanner Smith <tanner@tssoftware.net>
2012-12-28 16:27:04 -0500

    Draws circles on the basis of rings. Stores rings in array for later use.

--- js/number.js
+++ js/number.js
@@ -11,65 +11,40 @@ function GNumber(number) {
 		this.number = number.toString();
 	}
 
-	// Detect the decimal position, if it exists within the string
-	this.decimal_position = -1;
-
-	for (var i = 0; i < this.number.length; i++) {
-		if (this.number.charAt(i) == '.') {
-			this.decimal_position = i;
-		}
-	}
-
 	this.rings = [];
 
 	this.draw = function(paper, x, y, radius) {
-		var num_circles = this.number.length;
-
-		// If no decimal is present in the number, e.g. 10, then we need an extra circle
-		if (this.decimal_position < 0) {
-			num_circles++;
-		}
-
-		var spacing = (radius - radius * SMALLEST_RADIUS_RATIO) / num_circles;
+		var spacing = (radius - radius * SMALLEST_RADIUS_RATIO) / this.number.length;
+		var decimal_present = false;
 
 		var current_radius = radius;
 
-		// Draw the circles for the number from the outside in
-		var outer_circle = null;
-		var number;
-		for (var i = 0; i < num_circles; i++) {
-			if (this.number.charAt(i) == ',') {
-				continue;
-			}
-
-			var c = paper.circle(x, y, current_radius);
-
-			if (i == this.decimal_position) {
-				c.attr('stroke-width', BOLD_STROKE);
+		for (var i = 0; i < this.number.length; i++) {
+			if (this.number.charAt(i) == '.') {
+				// Decimal
+				decimal_present = true;
 			} else {
-				if  (i == num_circles - 1 && this.decimal_position == -1) {
-					c.attr('stroke-width', BOLD_STROKE);
-				} else {
+				// Number
+				var ring = {};
+
+				var c = paper.circle(x, y, current_radius);
+				c.attr('stroke-width', STROKE);
+
+				ring.circle = c;
+				ring.number = parseInt(this.number.charAt(i));
+
+				if (this.rings.length == 0) {
+					// This is the outer ring, we must draw two circles
+					current_radius -= spacing;
+					
+					var c = paper.circle(x, y, current_radius);
 					c.attr('stroke-width', STROKE);
 				}
+
+				this.rings.push(ring);
+
+				current_radius -= spacing;
 			}
-
-			if (number == null) {
-				number = parseInt(this.number.charAt(i));
-			}
-
-			if (outer_circle != null) {
-				this.rings.push({
-					inner: c,
-					outer: outer_circle,
-					number: number
-				});
-			}
-
-			number = this.number.charAt(i) != '.' ? parseInt(this.number.charAt(i)) : null;
-
-			current_radius -= spacing;
-			outer_circle = c;
 		}
 	}
 }
